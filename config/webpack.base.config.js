55713482f2b01153036f1aab163b1f2c6b232a7d
sjoedwards <samuel.edwards@sky.uk>
2018-12-18 21:17:33 +0000

    More changes

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,22 +1,41 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
-    })
-  ]
-}
+module.exports = env => {
+    return merge([
+        {
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: 'babel-loader'
+                        }
+                    },
+                    {
+                        test: /\.scss$/,
+                        use: [
+                            env.PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                            'css-loader',
+                            'sass-loader'
+                        ]
+                    },
+                ]
+            },
+            plugins: [
+                new HtmlWebpackPlugin({
+                    template: './src/index.html',
+                    filename: './index.html'
+                }),
+                new webpack.DefinePlugin({
+                    'process.env.VERSION': JSON.stringify(env.VERSION),
+                    'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
+                }),
+                new CopyWebpackPlugin([ { from: 'src/static' } ])
+            ]}
+    ]);
+};
