74e0087bfb8470041a1e003228395c95565de8bc
sjoedwards <samuel.edwards@sky.uk>
2018-12-28 00:25:26 +0000

    Browsified code

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,12 +1,20 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const APP_DIR = path.resolve(__dirname, '../src');
 
 module.exports = env => {
     return merge([
         {
+            entry: ['@babel/polyfill', APP_DIR],
+            output: {
+                path: path.resolve(__dirname, '../dist'),
+                publicPath: '/',
+                filename: '[name].js'
+            },
             module: {
                 rules: [
                     {
@@ -29,7 +37,8 @@ module.exports = env => {
             plugins: [
                 new HtmlWebpackPlugin({
                     template: './src/index.html',
-                    filename: './index.html'
+                    filename: './index.html',
+                    excludeChunks: [ 'server', 'routes' ]
                 }),
                 new webpack.DefinePlugin({
                     'process.env.VERSION': JSON.stringify(env.VERSION),
