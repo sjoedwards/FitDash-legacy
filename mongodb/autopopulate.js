3f5f1eb4fc804555193ad6368b473cf4dfdf061b
sjoedwards <samuel.edwards@sky.uk>
2019-05-22 10:41:39 +0100

    Added async autoimporter

--- mongodb/autopopulate.js
+++ mongodb/autopopulate.js
@@ -4,35 +4,35 @@ const options = {
     useNewUrlParser: true,
 };
 
-const autopopulate = async () => {
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function() {
-        console.log('connection open');
-    });
-    await db.dropCollection('kittens');
+const cycles = [{ _id: '1', name: 'Cycle 1' },{ _id: '2', name: 'Cycle 2' },{ _id: '3', name: 'Cycle 3' }];
 
-    var kittySchema = new mongoose.Schema({
-        _id: mongoose.Schema.Types.Mixed,
-        name: String
+var cycleSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.Mixed,
+    name: String
+});
+
+function dropCollection() {
+    return db.dropCollection('cycles');
+}
+
+const importCollection = (name, data, schema) => {
+    return data.map(async object => {
+        const Model = mongoose.model(name, schema);
+        const item = new Model(object);
+        return await item.save();
     });
-  
-    kittySchema.methods.speak = function () {
-        var greeting = this.name
-            ? 'Meow name is ' + this.name
-            : 'I don\'t have a name';
-        console.log(greeting);
-    };
-  
-    var Kitten = mongoose.model('Kitten', kittySchema);
-  
-    var silence = new Kitten({ _id: '1', name: 'Cat' });
-  
-    await silence.save();
-    db.close();
-  
 };
+
+async function autoImport() {
+    await dropCollection();
+    await Promise.all(importCollection('Cycles', cycles, cycleSchema));
+    db.close();
+}
 
 mongoose.connect('mongodb://root:root@mongo:27017/test?authSource=admin', options);
 var db = mongoose.connection;
-autopopulate();
-
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('connection open');
+});
+autoImport();
