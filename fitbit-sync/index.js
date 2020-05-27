7d023583def9411b6f1f2514d737f50354e3c6a2
sjoedwards <samuel.edwards@sky.uk>
2020-05-27 08:57:23 +0100

    Token refresh logic

--- fitbit-sync/index.js
+++ fitbit-sync/index.js
@@ -9,25 +9,10 @@ const moment = require('moment')
 const ObjectsToCsv = require('objects-to-csv')
 require('dotenv').config({ path: '../.env' })
 
-let accessToken;
-let accessCode;
-
-const getCode = async ctx => {
-  if (ctx.request.query.code) {
-    if ((ctx.request.query.code !== accessCode)) {
-      console.log('Obtained new code from query string')
-      accessCode = ctx.request.query.code
-      return accessCode
-    } else {
-      await ctx.redirect('/runs')
-    }
-  }
-}
-
-const getToken = async ctx => {
+const getTokens = async (ctx, accessCode) => {
   if (!accessCode) {
     console.log('No access code, redirecting to FitBit authZ')
-    return ctx.redirect(`https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&scope=activity`)
+    return ctx.redirect(`https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&scope=activity%20nutrition&test=test`)
   }
   const clientSecret = process.env.CLIENT_SECRET
   const clientId = process.env.CLIENT_ID
@@ -39,42 +24,80 @@ const getToken = async ctx => {
   try {
     const response = await axios({url: `https://api.fitbit.com/oauth2/token?code=${accessCode}&grant_type=authorization_code&client_id=${clientId}`, method: 'post', headers: headers})
     console.log('Successfully obtained token')
-    return response && response.data && response.data.access_token
+    return response && response.data
   } catch (e) {
     console.log('Failed to obtain token')
     console.log(e && e.response && e.response.data)
-  } finally {
-    console.log('Resetting accessCode')
-    accessCode = undefined
+  }
+}
+
+const refreshAccessToken = async refreshToken => {
+  const clientSecret = process.env.CLIENT_SECRET
+  const clientId = process.env.CLIENT_ID
+  const authString = btoa(`${clientId}:${clientSecret}`)
+  const headers = {
+    Authorization: `Basic ${authString}`,
+    "Content-Type": "application/x-www-form-urlencoded"
+  }
+  try {
+    const response = await axios({url: `https://api.fitbit.com/oauth2/token?grant_type=refresh_token&refresh_token=${refreshToken}`, method: 'post', headers: headers})
+    console.log('Successfully obtained token')
+    return response && response.data
+  } catch (e) {
+    console.log('Failed to obtain token')
+    console.log(e && e.response && e.response.data)
   }
 }
 
 const getActivities = async ctx => {
   const headers = {
-    Authorization: `Bearer ${accessToken}`
+    Authorization: `Bearer ${ctx.state.token}`
   }
   try {
-    return await axios({url: `https://api.fitbit.com/1/user/-/activities/list.json?beforeDate=${moment().add(1, 'days').format(("YYYY-MM-DD"))}&offset=1&limit=20&sort=desc`, method: 'get', headers})
+    return await axios({url: `https://api.fitbit.com/1/user/-/activities/list.json?beforeDate=${moment().add(1, 'days').format(("YYYY-MM-DD"))}&offset=0&limit=20&sort=desc`, method: 'get', headers})
   } catch (e) {
     console.log(e.message)
     console.log(e && e.response && e.response.data && e.response.data.errors)
   }
 }
 
+const getCalories = async (ctx, weeksAgo) => {
+  const headers = {
+    Authorization: `Bearer ${ctx.state.token}`
+  }
+  const weekStart = moment().subtract(weeksAgo, 'weeks').startOf('isoWeek').format("YYYY-MM-DD");
+  const weekEnd = moment().subtract(weeksAgo, 'weeks').endOf('isoWeek').format("YYYY-MM-DD");
+  const caloriesLog = (await axios({url: `https://api.fitbit.com/1/user/-/foods/log/caloriesIn/date/${weekStart}/${weekEnd}.json`, method: 'get', headers})).data["foods-log-caloriesIn"].filter(({value}) => value != 0)
+  const calories = (caloriesLog.reduce((sum, {value}) => sum + parseInt(value), 0) / caloriesLog.length).toFixed(0)
+  return {weekEnd, calories}
+}
+
 (async () => {
 
-  router.get('/authz', async ctx => {
-    accessCode = ctx.request.query.code
-    ctx.redirect('/runs')
+  router.get('/authn', async ctx => {
+    ctx.body = 'Completed authentication, attempt request again'
+  })
+
+  router.get('/calories', async ctx => {
+      try {
+
+          const calories = (await Promise.all(Array(6).fill(undefined).map(async (value, index) => {
+            const weeksAgo = index + 1;
+            return getCalories(ctx, weeksAgo)
+          }))).sort((a,b) => {
+            return a.weekEnd == b.weekEnd ? 0 : a.weekEnd > b.weekEnd ? 1 : -1
+          })
+          const csv = new ObjectsToCsv(calories)
+          await csv.toDisk(`./results/calories/${moment().format(("YYYY-MM-DD"))}.csv`);
+          ctx.body = await csv.toString()
+
+      } catch (e) {
+        console.log(e.message)
+        console.log(e && e.response && e.response.data && e.response.data.errors)
+      }
   })
 
   router.get('/runs', async ctx => {
-    if (accessToken) {
-      console.log('Token obtained from memory')
-    } else {
-      accessToken = await getToken(ctx)
-    }
-    if (accessToken) {
       const activities = await getActivities(ctx)
       if (activities && activities.data && activities.data.activities) {
         const formattedActivities = activities.data.activities.filter(activity => activity.activityName == 'Run').map(({distance, pace, originalStartTime, activeDuration}) => {
@@ -84,19 +107,40 @@ const getActivities = async ctx => {
             duration: moment.utc(moment.duration((activeDuration), "milliseconds").asMilliseconds()).format("HH:mm:ss"),
             pace: moment.utc(moment.duration(pace, "seconds").asMilliseconds()).format("HH:mm:ss")
           }
+        }).sort((a,b) => {
+          return a.date == b.date ? 0 : a.date > b.date ? 1 : -1
         })
       const csv = new ObjectsToCsv(formattedActivities)
-      await csv.toDisk(`./results/${moment().format(("YYYY-MM-DD"))}-runs.csv`);
+      await csv.toDisk(`./results/runs/${moment().format(("YYYY-MM-DD"))}.csv`);
       ctx.body = await csv.toString()
       }
-    }
-    if (ctx.status == 302) {
-      return
-    } else {
-      ctx.status = 400
-    }
   });
+
   app
+  .use(async (ctx, next) => {
+    const accessToken = await ctx.cookies.get('accessToken');
+    const refreshToken = await ctx.cookies.get('refreshToken');
+    if (accessToken) {
+      console.log('Token obtained from cookie')
+      ctx.state.token = accessToken
+    } else if (refreshToken) {
+        console.log('Refreshing token')
+        const tokens = await refreshAccessToken(refreshToken);
+        tokens && ctx.cookies.set('accessToken', tokens.access_token, {maxAge: tokens.expires_in})
+        ctx.state.token = tokens && tokens.access_token
+      }
+    if (!ctx.state.token) {
+      accessCode = ctx.request.query.code
+      const tokens = await getTokens(ctx, accessCode)
+      if (ctx.status === 302) {
+        return
+      }
+      ctx.cookies.set('accessToken', tokens.access_token, {maxAge: tokens.expires_in})
+      ctx.cookies.set('refreshToken', tokens.refresh_token)
+      ctx.redirect('/authn')
+    }
+    return next()
+  })
   .use(router.routes())
   .use(router.allowedMethods());
   app.listen(3005);
